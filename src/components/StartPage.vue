83653cf2727880b140de04fee36926fa28fd403f
emiliamassing <emiliamassings@gmail.com>
2023-05-29 13:49:46 +0200

    Added startpage component

---
+++ src/components/StartPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+    <h3>Who's There? - Animal edition</h3>
+    <button>Start game</button>
+</template>
+
+<style>
+    button {
+        background-color: rgb(255, 159, 63);
+    }
+</style>
